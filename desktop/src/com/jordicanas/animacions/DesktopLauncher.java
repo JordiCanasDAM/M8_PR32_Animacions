22046acf632309950bca62cf0f6134eff6222063
Jordi <jcanasalvarez.cf@iesesteveterradas.cat>
2023-04-11 17:00:37 +0200

    Initial commit

---
+++ desktop/src/com/jordicanas/animacions/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.jordicanas.animacions;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.jordicanas.animacions.Animacions;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Animacions");
+		new Lwjgl3Application(new Animacions(), config);
+	}
+}
