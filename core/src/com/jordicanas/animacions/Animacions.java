df52ebe8fef060b469a61cab6f6df031d0d3b38c
Jordi <jcanasalvarez.cf@iesesteveterradas.cat>
2023-04-17 17:43:06 +0200

    Sprites mides diferents

--- core/src/com/jordicanas/animacions/Animacions.java
+++ core/src/com/jordicanas/animacions/Animacions.java
@@ -14,18 +14,27 @@ public class Animacions extends ApplicationAdapter {
 	private static final int FRAME_COLS = 6, FRAME_ROWS = 5;
 
 	// Objects used
-	Animation<TextureRegion> walkAnimation; // Must declare frame type (TextureRegion)
 	Texture walkSheet;
+	Animation<TextureRegion> walkAnimation; // Must declare frame type (TextureRegion)
+
+
+	//Sprite afegit
+	Texture walkPrinceOfPersia;
+	Animation<TextureRegion> princeOfPersia;
+
 	SpriteBatch spriteBatch;
 
 	// A variable for tracking elapsed time for the animation
 	float stateTime;
+
+
 
 	@Override
 	public void create() {
 
 		// Load the sprite sheet as a Texture
 		walkSheet = new Texture(Gdx.files.internal("animation_sheet.png"));
+		walkPrinceOfPersia = new Texture(Gdx.files.internal("PrinceOfPersia_Walking.png"));
 
 		// Use the split utility method to create a 2D array of TextureRegions. This is
 		// possible because this sprite sheet contains frames of equal size and they are
@@ -33,7 +42,6 @@ public class Animacions extends ApplicationAdapter {
 		TextureRegion[][] tmp = TextureRegion.split(walkSheet,
 				walkSheet.getWidth() / FRAME_COLS,
 				walkSheet.getHeight() / FRAME_ROWS);
-
 		// Place the regions into a 1D array in the correct order, starting from the top
 		// left, going across first. The Animation constructor requires a 1D array.
 		TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
@@ -44,8 +52,29 @@ public class Animacions extends ApplicationAdapter {
 			}
 		}
 
+		TextureRegion animationFrames[] = new TextureRegion[16];
+		animationFrames[0] = new TextureRegion(walkPrinceOfPersia,0,0,13,53);
+		animationFrames[1] = new TextureRegion(walkPrinceOfPersia,24,0,34-24,53);
+		animationFrames[2] = new TextureRegion(walkPrinceOfPersia,48,0,60-48,53);
+		animationFrames[3] = new TextureRegion(walkPrinceOfPersia,72,0,88-72,53);
+		animationFrames[4] = new TextureRegion(walkPrinceOfPersia,96,0,113-96,53);
+		animationFrames[5] = new TextureRegion(walkPrinceOfPersia,120,0,140-120,53);
+		animationFrames[6] = new TextureRegion(walkPrinceOfPersia,152,0,176-152,53);
+		animationFrames[7] = new TextureRegion(walkPrinceOfPersia,184,0,208-184,53);
+		animationFrames[8] = new TextureRegion(walkPrinceOfPersia,216,0,239-216,53);
+		animationFrames[9] = new TextureRegion(walkPrinceOfPersia,247,0,281-247,53);
+		animationFrames[10] = new TextureRegion(walkPrinceOfPersia,287,0,316-287,53);
+		animationFrames[11] = new TextureRegion(walkPrinceOfPersia,327,0,353-327,53);
+		animationFrames[12] = new TextureRegion(walkPrinceOfPersia,359,0,385-359,53);
+		animationFrames[13] = new TextureRegion(walkPrinceOfPersia,392,0,427-392,53);
+		animationFrames[14] = new TextureRegion(walkPrinceOfPersia,439,0,467-439,53);
+		animationFrames[15] = new TextureRegion(walkPrinceOfPersia,480,0,500-480,53);
+
+
 		// Initialize the Animation with the frame interval and array of frames
 		walkAnimation = new Animation<TextureRegion>(0.025f, walkFrames);
+		princeOfPersia = new Animation<TextureRegion>(0.1f,animationFrames);
+
 
 		// Instantiate a SpriteBatch for drawing and reset the elapsed animation
 		// time to 0
@@ -60,8 +89,10 @@ public class Animacions extends ApplicationAdapter {
 
 		// Get current frame of animation for the current stateTime
 		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+		TextureRegion spriteFrame = princeOfPersia.getKeyFrame(stateTime,true);
 		spriteBatch.begin();
 		spriteBatch.draw(currentFrame, 50, 50, 500, 500); // Draw current frame at (50, 50)
+		spriteBatch.draw(spriteFrame, 50, 50);
 		spriteBatch.end();
 	}
 
